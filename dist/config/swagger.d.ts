ede3e884674267942b00dd0b7a00acf57203bfb5
Sergio Alejandro Gonzalez Sánchez <82124793+sergio3144@users.noreply.github.com>
2024-12-10 16:02:58 -0500

    translate compilation v3

--- dist/config/swagger.d.ts
+++
@@ -1,2 +0,0 @@
-declare const swaggerSpec: any;
-export default swaggerSpec;
--- dist/controllers/users.controllers.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-export declare const getUsers: (req: Request, res: Response) => Promise<void>;
-export declare const getUserById: (req: Request, res: Response) => Promise<any>;
-export declare const createUser: (req: Request, res: Response) => Promise<any>;
-export declare const deleteUser: (req: Request, res: Response) => Promise<any>;
-export declare const updateUserPut: (req: Request, res: Response) => Promise<any>;
