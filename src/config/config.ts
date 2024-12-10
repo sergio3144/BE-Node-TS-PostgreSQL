999ef0a2c0a0face1d5b39538ab032aadcc2afd7
Sergio Alejandro Gonzalez Sánchez <82124793+sergio3144@users.noreply.github.com>
2024-12-10 15:39:11 -0500

    .env global and variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const DB_USER = 'postgres'
-export const DB_HOST = 'localhost'
-export const DB_PASSWORD = '3144DBS'
-export const DB_DATABASE = 'nodepg'
-export const DB_PORT = 5432
+export const DB_USER = process.env.DB_USER
+export const DB_HOST = process.env.DB_HOST 
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_PORT = parseInt(process.env.DB_PORT || '5432')
 
 
 export const PORT = process.env.PORT || 3000
